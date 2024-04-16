c6f2594887fee8e5d91854670999c9210bd08a70
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-04-16 23:44:04 +0100

    add lint script  to cww-ui add lint script to cww-ui also make sure typescript version is not greater than 5.4.0 because of an annoying @typescript-eslint  requirement //TODO change the typescript version back to latest later

--- packages/cww-ui/.eslintrc.js
+++ packages/cww-ui/.eslintrc.js
@@ -1,9 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
+  extends: ["@codewithwalid/eslint-config/react.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
   },
+  ignorePatterns: ["*.config.js", "*.config.ts"],
+  
 };
